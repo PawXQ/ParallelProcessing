b8bc2db9f79aa8fd24e18634568336474c8d587b
PawXQ <pawxq@mail.com>
2026-04-20 21:35:06 +0800

    use lock to write single file

--- ParallelProcessing/Program.cs
+++ ParallelProcessing/Program.cs
@@ -1,21 +1,25 @@
 ﻿using CSVLibrary;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ParallelProcessing
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            object key = new object();
+
             const int BATCH_QUANTITY = 2_500_000;
-            const int ROW_DATA = 10_000_000;
+            const int ROW_DATA = 13_000_000;
             const int BATCH = ROW_DATA % BATCH_QUANTITY == 0 ? ROW_DATA / BATCH_QUANTITY : ROW_DATA / BATCH_QUANTITY + 1;
 
             string path = @"C:\Users\Albert\Github\repos\private\c_sharp\leo_class\console\ParallelProcessingData";
@@ -26,51 +30,47 @@ namespace ParallelProcessing
                 File.Delete(writePath);
 
 
-            //Stopwatch sw = new Stopwatch();
-            //sw.Start();
-
-            //List<Record> record_list = CSVHelper.Read<Record>(readPath);
-
-            //sw.Stop();
-            //double swRead = sw.ElapsedMilliseconds / 1000.0;
-            //Console.WriteLine(swRead);
-
-            //sw.Restart();
-
-            //CSVHelper.WriteList(writePath, record_list, true);
-
-            //double swWrite = sw.ElapsedMilliseconds / 1000.0;
-            //Console.WriteLine(swWrite);
-
-            //Console.WriteLine(swRead + swWrite);
-
             List<double> readTimes = new List<double>();
             List<double> writeTimes = new List<double>();
+
+            List<Task> tasks = new List<Task>();
 
             Stopwatch swTotal = new Stopwatch();
             swTotal.Start();
 
             for (int i = 0; i < BATCH; i++)
             {
+                int index = i;
                 Stopwatch sw = new Stopwatch();
-                int start = i * BATCH_QUANTITY + 1;
 
-                sw.Start();
-                List<Record> record_list = CSVHelper.Read<Record>(readPath, start, BATCH_QUANTITY);
-                sw.Stop();
-                double swRead = sw.ElapsedMilliseconds / 1000.0;
-                readTimes.Add(swRead);
-                Console.WriteLine($"Batch{i + 1} read: {swRead}");
+                Task taskRW = Task.Run(() =>
+                {
+                    int start = index * BATCH_QUANTITY + 1;
 
-                sw.Restart();
-                CSVHelper.WriteList(writePath, record_list, true);
-                sw.Stop();
-                double swWrite = sw.ElapsedMilliseconds / 1000.0;
-                writeTimes.Add(swWrite);
-                Console.WriteLine($"Batch{i + 1} write: {swWrite}");
+                    sw.Start();
+                    List<Record> record_list = CSVHelper.Read<Record>(readPath, start, BATCH_QUANTITY);
+                    sw.Stop();
+                    double swRead = sw.ElapsedMilliseconds / 1000.0;
+                    readTimes.Add(swRead);
+                    Console.WriteLine($"Batch{index + 1} read: {swRead}");
 
-                Console.WriteLine($"Batch{i + 1} total: {swRead + swWrite}");
+                    sw.Restart();
+                    lock (key)
+                    {
+                        CSVHelper.WriteList(writePath, record_list, true);
+                    }
+                    sw.Stop();
+                    double swWrite = sw.ElapsedMilliseconds / 1000.0;
+                    writeTimes.Add(swWrite);
+                    Console.WriteLine($"Batch{index + 1} write: {swWrite}");
+
+                    Console.WriteLine($"Batch{index + 1} total: {swRead + swWrite}");
+                });
+
+                tasks.Add(taskRW);
             }
+
+            await Task.WhenAll(tasks);
 
             swTotal.Stop();
             Console.WriteLine(swTotal.ElapsedMilliseconds / 1000.0);
@@ -82,9 +82,6 @@ namespace ParallelProcessing
             Console.WriteLine($"WriteMediam: {writedMedian}");
 
             Console.WriteLine($"|  {BATCH_QUANTITY.ToString("#,##0")}     | {ROW_DATA.ToString("#,##0")}     |{Math.Round(readTimes.Median(x => x), 2)}            |     {Math.Round(writeTimes.Median(x => x), 2)}            |     {Math.Round(swTotal.ElapsedMilliseconds / 1000.0, 2)}          |                |");
-            //Console.WriteLine($"|  {batchcount.ToString("#,##0")}     | {ROW_COUNT.ToString("#,##0")}     |{Math.Round(EnumerableExtension.Median(readTimes), 2)}            |     {Math.Round(EnumerableExtension.Median(writeTimes), 2)}            |     {Math.Round(totalStopwatch.ElapsedMilliseconds / 1000.0, 2)}          |                |");
-
-
 
             Console.ReadKey();
         }
